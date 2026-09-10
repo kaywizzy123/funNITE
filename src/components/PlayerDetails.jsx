09384a7e2d2c5cd81e8ddf675e323e36cbb68603
Oluwakayode Ogunremi <kayodeogunremi33@gmail.com>
2026-09-10 11:21:52 -0700

    Added the add and delete functionality for player details

--- src/components/PlayerDetails.jsx
+++ src/components/PlayerDetails.jsx
@@ -1,18 +1,37 @@
 import { Minus, User } from "lucide-react";
 
-export default function PlayerDetails({ id, name }) {
+export default function PlayerDetails({
+  id,
+  name,
+  count,
+  players,
+  setPlayers,
+  setErr,
+}) {
+  function handleDelete(id) {
+    if (players.length === 3) {
+      setErr("You can't create a fixtures with less than 3 players");
+      return null;
+    }
+
+    setErr("");
+    setPlayers((prev) => prev.filter((player) => player.id !== id));
+  }
   return (
     <div className="flex justify-between items-center gap-2 w-full">
-      <p className="flex justify-center items-center text-lg">{id}</p>
+      <p className="flex justify-center items-center text-lg">{count}.</p>
       <input
         type="text"
         className="flex-1 bg-neutral-50/10 rounded-4xl px-3.5 py-2 outline-none focus:ring-1 focus:ring-blue-500"
-        placeholder={`Enter ${name} name...`}
+        placeholder={`Enter player name...`}
       />
       <div className="flex justify-center items-center w-10 h-10 bg-neutral-300/30 border border-neutral-400/70 rounded-full ">
         <User className="text-neutral-400" />
       </div>
-      <div className="flex justify-center items-center w-6 h-6 transition-all duration-300 rounded-full bg-red-500 hover:scale-115 active:scale-95 hover:bg-red-700 active:bg-red-300">
+      <div
+        onClick={() => handleDelete(id)}
+        className="flex justify-center items-center w-6 h-6 transition-all duration-300 rounded-full bg-red-500 hover:scale-115 active:scale-95 hover:bg-red-700 active:bg-red-300"
+      >
         <Minus size={20} />
       </div>
     </div>
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,67 +1,95 @@
+import { useEffect, useState } from "react"; // 1. Import useState
 import { ChevronDown, Plus } from "lucide-react";
 import PlayerDetails from "../components/PlayerDetails";
 import { Link } from "react-router-dom";
 
 export default function Create() {
-  const players = [
-    { id: 1, name: "Player 1" },
-    { id: 2, name: "Player 2" },
-    { id: 3, name: "Player 3" },
-  ];
+  const [err, setErr] = useState("");
+  const [players, setPlayers] = useState([
+    { id: crypto.randomUUID(), name: "" },
+    { id: crypto.randomUUID(), name: "" },
+    { id: crypto.randomUUID(), name: "" },
+  ]);
+
+  useEffect(() => {
+    if (!err) return;
+    const timeout = setTimeout(() => setErr(""), 3000);
+    return () => clearTimeout(timeout);
+  }, [err]);
+
+  function handleAdd() {
+    setErr("");
+    setPlayers((prevPlayers) => {
+      return [
+        ...prevPlayers,
+        {
+          id: crypto.randomUUID(),
+          name: "",
+        },
+      ];
+    });
+  }
+
   return (
-    <div className="flex justify-center  flex-1 text-center mx-2 md:mx-0">
+    <div className="flex justify-center flex-1 text-center mx-2 md:mx-0">
       <div className="flex flex-col m-4 w-full gap-4 items-center">
         <h1 className="text-3xl md:text-5xl font-bold">Create New Game</h1>
         <div className="flex flex-col w-full justify-center items-center">
           <fieldset className="border border-neutral-50/6 bg-neutral-700/10 rounded-2xl w-full md:w-xl">
             <legend>Enter Players details</legend>
 
-            <form className="flex flex-col gap-2 justify-center items-center m-5">
+            <div className="flex flex-col gap-2 justify-center items-center m-5">
               <div className="w-full flex justify-between items-center gap-2 mb-3">
-                <div class="relative w-full max-w-xs">
-                  <select class="w-full appearance-none rounded-full bg-blue-500 px-4 py-2.5 pr-10 text-sm text-neutral-50 shadow-sm transition-all focus:border-none focus:outline-none focus:ring-0 focus:ring-indigo-100">
-                    <option value="">Knockout</option>
-                    <option value="">League</option>
+                <div className="relative w-full max-w-xs">
+                  <select className="w-full appearance-none rounded-full bg-blue-500 px-4 py-2.5 pr-10 text-sm text-neutral-50 shadow-sm transition-all focus:border-none focus:outline-none focus:ring-0 focus:ring-indigo-100">
+                    <option value="knockout">Knockout</option>
+                    <option value="league">League</option>
                   </select>
-
-                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-neutral-50">
-                    <ChevronDown />
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                    />
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-neutral-50">
+                    <ChevronDown size={18} />
                   </div>
                 </div>
 
-                <div class="relative w-full max-w-xs">
-                  <select class="w-full appearance-none rounded-full bg-blue-500 px-4 py-2.5 pr-10 text-sm text-neutral-50 shadow-sm transition-all focus:border-none focus:outline-none focus:ring-0 focus:ring-indigo-100">
-                    <option value="">Single</option>
-                    <option value="">Home & Away</option>
+                <div className="relative w-full max-w-xs">
+                  <select className="w-full appearance-none rounded-full bg-blue-500 px-4 py-2.5 pr-10 text-sm text-neutral-50 shadow-sm transition-all focus:border-none focus:outline-none focus:ring-0 focus:ring-indigo-100">
+                    <option value="single">Single</option>
+                    <option value="home_away">Home & Away</option>
                   </select>
-
-                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-neutral-50">
-                    <ChevronDown />
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                    />
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-neutral-50">
+                    <ChevronDown size={18} />
                   </div>
                 </div>
 
-                <div className="bg-blue-500 rounded-full p-1 transition-all duration-300 hover:bg-blue-700 hover:scale-105 active:scale-95">
+                <button
+                  type="button"
+                  onClick={handleAdd}
+                  className="bg-blue-500 rounded-full p-1 transition-all duration-300 hover:bg-blue-700 hover:scale-105 active:scale-95 cursor-pointer"
+                >
                   <Plus />
-                </div>
+                </button>
               </div>
-              {players.map((player) => (
-                <PlayerDetails id={player.id} name={player.name} />
-              ))}
-            </form>
+              <p className="text-red-400">{err}</p>
+              <p className="text-neutral-500">
+                Number of Players: {players.length}
+              </p>
+              <div className="w-full flex flex-col gap-2 max-h-144 overflow-auto">
+                {players.map((player, index) => (
+                  <PlayerDetails
+                    key={player.id}
+                    id={player.id}
+                    name={player.name}
+                    count={index + 1}
+                    setPlayers={setPlayers}
+                    players={players}
+                    setErr={setErr}
+                  />
+                ))}
+              </div>
+            </div>
           </fieldset>
         </div>
         <Link
-          to="/create"
+          to="/fixtures"
           className="flex items-center justify-center gap-0.5 bg-blue-500 px-10 py-1.5 rounded-4xl transition-all duration-300 hover:scale-105 active:scale-95 text-2xl"
         >
           Generate Fixtures
