203d422d684f57c385cdb7b5ccb2507e1abe65a7
Oluwakayode Ogunremi <kayodeogunremi33@gmail.com>
2026-09-10 11:50:45 -0700

    adjust some UI looks in create.jsx

--- src/components/PlayerDetails.jsx
+++ src/components/PlayerDetails.jsx
@@ -19,10 +19,12 @@ export default function PlayerDetails({
   }
   return (
     <div className="flex justify-between items-center gap-2 w-full">
-      <p className="flex justify-center items-center text-lg">{count}.</p>
+      <p className="flex justify-center items-center w-8 h-8 text-neutral-600 text-lg">
+        {count}.
+      </p>
       <input
         type="text"
-        className="flex-1 bg-neutral-50/10 rounded-4xl px-3.5 py-2 outline-none focus:ring-1 focus:ring-blue-500"
+        className="flex-1 bg-neutral-50/10  rounded-4xl px-3.5 py-2 outline-none focus:ring-1 focus:ring-blue-500"
         placeholder={`Enter player ${count} name...`}
       />
       <div className="flex justify-center items-center w-10 h-10 bg-neutral-300/30 border border-neutral-400/70 rounded-full ">
